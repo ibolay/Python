e9fa9161ab77f84b27ca0729e192cee301b65540
ibolicyber <ibrahimcyber4@gmail.com>
2026-01-22 20:27:08 +0400

    6 count()

---
+++ 6 count()/1.py
@@ -0,0 +1,8 @@
+
+
+cumle = "salam ibrahim netersen yaxsisanmi , birdaha salam"
+
+print( cumle.count('i') )     # i herfinin sayi
+print( cumle.count('n') )     # n herfinin sayi
+print( cumle.count(' ') )     # bosluq
+
