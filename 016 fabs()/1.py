20730fbea64a8b0278c4d57df5772e48f1504503
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-12 16:14:49 +0400

    016 fabs()

---
+++ 016 fabs()/1.py
@@ -0,0 +1,8 @@
+# fabs() – bu funksiya "abs()" ilə eyni işi görür.
+# Fərq ondadır ki, "fabs()" nəticəni onluq ədəd (float) kimi qaytarır.
+# Bu funksiya math moduluna aiddir.
+
+import math
+eded = -12
+
+print( math.fabs(eded) )
---
+++ 016 fabs()/2.py
@@ -0,0 +1,6 @@
+# aradaki fərg :
+
+import math
+
+print( abs( -5 ) )        # 5
+print( math.fabs( -5 ) )  # 5.0
