3a68257b4aa9fccfecd22b4d4f00aeb12b1baf5a
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-21 09:27:44 +0400

    1 - Arithmetic Operators

---
+++ 023 operators/1 - Arithmetic Operators/1.py
@@ -0,0 +1,24 @@
+# 1 - Arithmnetic Operators 
+
+
+# Arithmnetic Operators 
+   # a)  +       - Toplama
+   # b)  -       - Çıxma
+   # c)  *       - Vurma
+   # d)  /       - Bölme
+   # e)  %       - Qalıqlı Bölme
+   # f)  **      - Ededin Üstü
+   # g)  //      - Qalıqsız Bölme
+
+
+a = 6
+b = 2
+
+print( a + b  )   #  8
+print( a - b  )   #  4
+print( a * b  )   #  12
+print( a / b  )   #  3.0
+print( a % b  )   #  0
+print( a ** b )   #  36
+print( a // b )   #  3
+
