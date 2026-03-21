4acdc4cf5edc3b5a5497d8d2f06ef05255251113
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-21 09:14:49 +0400

    022 slice()

---
+++ 022 slice()/1.py
@@ -0,0 +1,7 @@
+# slice() - index[] ilə eyni iş prinsipinə malikdir.
+# 
+
+cumle = "Kitab oxumaq ən faydali məşğuliyyətlərdən biridir."
+fraqman = slice(6 , 16)
+
+print( cumle [ fraqman ] )   # oxumaq ən 
