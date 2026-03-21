7b3fd2844275fa03861dac9478aec206bc110fb1
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-21 10:26:19 +0400

    2 - Assignment Operators

---
+++ 023 operators/2 - Assignment Operators/1.py
@@ -0,0 +1,26 @@
+# 1 - Assignment Operators - Qısaltma Operatoru
+
+
+# Assignment Operators
+   # a)    =          x = 5
+   # b)   +=          x += 5
+   # c)   -=          x -= 5
+   # d)   *=          x *= 5
+   # e)   /=          x /= 5
+   # f)   %=          x %= 5
+   # g)  **=          x **= 5
+   # h)  //=          x //= 5
+   # i)   &=          x &= 5
+   # j)   |=          x |= 5
+   # k)   ^=          x ^= 5
+   # l)  >>=          x >>= 5
+   # m)  <<=          x <<= 5
+
+
+a = 5
+b = 10
+
+a += b
+
+print ( a )   #  15 
+
