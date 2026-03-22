5e36dd5210ab689d1868dc7a983d61a2f41b9d1b
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-22 11:10:44 +0400

    3 - Comparison Operators

---
+++ 023 operators/3 - Comparison Operators/1.py
@@ -0,0 +1,22 @@
+# 3 - Comparison Operators - Müqayisə Operatoru
+
+
+# Comparison Operators
+   # a)    ==        
+   # b)    !=         
+   # c)    >         
+   # d)    <        
+   # e)    >=          
+   # f)    <=       
+
+
+x = 5
+y = 10
+
+print( x == y )     # False
+print( x != y )     # True
+print( x > y  )     # False
+print( x < y  )     # True 
+print( x >= y )     # False
+print( x <= y )     # True 
+  
