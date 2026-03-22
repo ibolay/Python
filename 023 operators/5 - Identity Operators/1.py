a9079ed8640dd9c38248c5aab4c37667897d9327
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-22 11:52:48 +0400

    5 - İdentity Operators

---
+++ 023 operators/5 - Identity Operators/1.py
@@ -0,0 +1,14 @@
+# 5 - Identity Operators 
+
+
+# Identity Operators
+   # a)   is        - Her iki variable doğrudursa, "True" qayıdır.
+   # b)   is not    - Her iki variable doğru deyilsə, "False" qayıdır.
+
+x = [ "apple" , "banana" ]
+y = [ "apple" , "banana" ]
+
+z = x
+
+print( x is z     )     # True qayıdır Çünkü x ilə z eynidir
+print( x is not z )     # False qayıdır Çünkü x ilə z eynidir
