8301800a314cf1777b6046861978b1598e0b68e7
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-22 14:48:15 +0400

    025 while

---
+++ 025 while/1.py
@@ -0,0 +1,6 @@
+# While - şərt qoşanda istifadə olunur, While sadəcə True qayıdanda işləyir.
+# Sonsuz dövr yaratmaq üçün lazımdır. While ile işleyerken ehtiyatlı olmaq lazımdır
+# Çünkü sonsuz dövr yaranır ve buda yaddaşı doldurur, proqram ilişə Komputer zerer göre biler.
+
+while 1 == 1:
+    print("sonsuz dövr")
---
+++ 025 while/2.py
@@ -0,0 +1,8 @@
+# Heç bir şey daxil etməsək, dövr davam edir; dəyər daxil etdikdə dayandırılır
+
+ad = ""
+
+while not ad:
+    ad = input("Ad daxil edin: ")
+
+print("Sizin adiniz: " + ad)
