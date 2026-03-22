2a8accccdd62bb8be718fce39d6836393556bd9d
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-22 11:59:47 +0400

    6 - Membership Operators

---
+++ 023 operators/6 - Membership Operators/1.py
@@ -0,0 +1,12 @@
+# 5 - Membership Operators 
+
+
+# Membership Operators
+   # a)   in        - Əğər axtarılan deyer varsa, "True" qaytarır.
+   # b)   not in    - Əğər axtarılan deyer yoxdursa, "False" qayıdır.
+
+x = [ "apple" , "banana" ]
+
+print( "apple" not in x   )     # False qayıdır Çünkü "aplle" deyeri "x" in içində var.
+print( "banana" in x      )     # True qayıdır Çünkü "banana" "x" in içində var.
+
