d292b3c4aebce9bcc592e3176343d1b349f4e61f
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-22 12:13:24 +0400

    Bitwise Operators

---
+++ 023 operators/7 - Bitwise Operators/1.py
@@ -0,0 +1,21 @@
+# 7 - Bitwise Operators 
+
+"""
+
+Bitwise operatorlar ədədləri binary (0 və 1) formasinda işləyərək əməliyyat aparir. 
+Yəni bu operatorlar ədədlərin özünə yox, onlarin bitlərinə təsir edir.
+
+"""
+
+# Bitwise Operators
+     # a)   &=          x &= 5     AND (hər iki bit 1 olarsa 1 olur).
+     # b)   |=          x |= 5     OR (bitlərdən biri 1 olsa kifayətdir).
+     # c)   ^=          x ^= 5     XOR (bitlər fərqlidirsə 1 olur).
+     # d)  >>=          x >>= 5    bitləri sağa sürüşdürür.
+     # e)  <<=          x <<= 5    bitləri sola sürüşdürür.
+
+x = 20
+x >> 5
+ 
+print ( x )     # --> 20
+
