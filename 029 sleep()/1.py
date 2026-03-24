933141f3f6940641686195fa0ad7c12888fa683e
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-24 22:24:34 +0400

    029 sleep()

---
+++ 029 sleep()/1.py
@@ -0,0 +1,11 @@
+# daha once math modulunu gormustuk, indi ise time modulu ile isleyeceyiq
+# sleep() - ile vaxt teyin ediriq.
+
+import time
+
+for saniye in range(10, 0, -1):
+    print(saniye)
+    time.sleep(1)
+
+print('Film Basladi')    
+
