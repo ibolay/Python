5b3b60d7cb9f8307a05756729847d47f81526a0f
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-24 22:19:11 +0400

    028 SIRASINA GORE NETİCE

---
+++ 028 SIRASINA GÖRE NETİCE/1.py
@@ -0,0 +1,13 @@
+say1 = int( input( 'Nece defe? ' ) )
+say2 = int( input( 'Nece defe? ' ) )
+simvol = input( 'Hansi simvol? ' )
+
+for i in range(say1):
+
+    for x in range(say2):
+
+        print(simvol)
+
+    print()
+
+    
