21d2d975404012f663d93d548ade5a7fc96a21dd
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-24 22:34:01 +0400

    030 break

---
+++ 030 break/1.py
@@ -0,0 +1,11 @@
+# break - sındırmaq demekdir, while ile yaranan donguyu sonlandiraq
+# Heçne daxil etmedikde dongu devam edir, birşey yazdıqda deyanır.
+
+while True:
+
+    ad = input( 'Adinizi daxil edin: ' )
+
+    if ad != '':
+
+        break
+
