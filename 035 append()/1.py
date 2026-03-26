4de14a64782139fd18ca86dd92313cf527a25b37
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-26 23:45:11 +0400

    035 append()

---
+++ 035 append()/1.py
@@ -0,0 +1,10 @@
+# append() = bu metod listin sonuna deyer elave etmek uçun istifade edilir.
+
+meyve = [ "qarpiz", "yemis", "alma", "nar" ]
+
+meyve.append("armud")
+
+for yemekler in meyve:
+    print(yemekler)
+
+    
