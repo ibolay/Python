1e7e75ea237c8a9abf976231cd7e20bfff3292f8
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-02 22:43:39 +0400

    037 pop()

---
+++ 037 pop()/1.py
@@ -0,0 +1,18 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  pop() - listin içinden deyer silmek üçün istifadə         ┃
+# ┃  edilir. Lakin bu metodun içində string deyerler yox       ┃
+# ┃  int deyerler yazılır.                                     ┃
+# ┃                                                            ┃
+# ┃  heçne yazmasaq en sonuncu deyer silinir.                  ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyve = ["alma", "armud", "nar", "heyva"]
+meyve.pop()
+
+print(meyve)
+
+# Output:
+# ['alma', 'armud', 'nar']
---
+++ 037 pop()/2.py
@@ -0,0 +1,16 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  pop() - listin içinden deyer silmek üçün istifadə         ┃
+# ┃  edilir. Lakin bu metodun içində string deyerler yox       ┃
+# ┃  int deyerler yazılır.                                     ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyve = ["alma", "armud", "nar", "heyva"]
+meyve.pop(2)
+
+print(meyve)
+
+# Output:
+# ['alma', 'armud', 'heyva']
