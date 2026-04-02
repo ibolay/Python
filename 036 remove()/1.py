c07d9796c638a0b40f30fb68fabf2c348ed5e917
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-02 22:35:30 +0400

    036 remove()

---
+++ 036 remove()/1.py
@@ -0,0 +1,17 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  remove() - listin içinden deyer silmek üçün               ┃
+# ┃  istifadə edilir. Olmuyan deyeri silmek istedikde          ┃
+# ┃                                                            ┃
+# ┃  olmuyan deyeri silmek istesek xeta mesajı alarıq          ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyve = ["alma", "armud", "nar", "heyva"]
+meyve.remove("alma")
+
+print(meyve)
+
+# Output:
+# ['armud', 'nar', 'heyva']
