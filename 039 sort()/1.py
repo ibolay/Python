32ef8baf7b7598dc3241b53568bf115af1a47424
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-03 19:57:31 +0400

    039 sort()

---
+++ 039 sort()/1.py
@@ -0,0 +1,21 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  sort() - listin içindeki deyerleri elifba sırasına        ┃
+# ┃  görə sıralıyır.                                           ┃
+# ┃                                                            ┃
+# ┃  yeni siyahı yaranmır mövcüd siyahı deyişdirilir.          ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyve = ["alma", "heyva", "nar", "armud"]
+meyve.sort()
+
+for deyer in meyve:
+    print(deyer)
+
+# Output:
+# alma
+# armud
+# heyva
+# nar
