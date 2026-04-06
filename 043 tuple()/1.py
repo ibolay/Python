d2898c94ed9ef5242686942a00ffedc0ca8f754c
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-06 20:56:43 +0400

    043 tuple() - 1

---
+++ 043 tuple()/1.py
@@ -0,0 +1,14 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  tuple() - yaratmaq üçün yumru möterizeden istifade        ┃
+# ┃  edilir. tuple - dest demekdir.                            ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyveler = ("alma", "banan", "portağal")
+
+print(meyveler)
+
+# Output:
+# ('alma', 'banan', 'portağal')
---
+++ 043 tuple()/2.py
@@ -0,0 +1,14 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  tuple - yaratmaq üçün tuple() metodundanda istifadə       ┃
+# ┃  edilə bilər.                                              ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyveler = tuple(  ("alma", "banan", "portağal")  )
+
+print(meyveler)
+
+# Output:
+# ('alma', 'banan', 'portağal')
