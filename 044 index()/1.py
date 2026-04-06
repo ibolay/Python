1657b6e5032cd09594e25801cc41bd73d4b1f30e
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-06 21:13:23 +0400

    044 index()

---
+++ 044 index()/1.py
@@ -0,0 +1,16 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  index() - tuple içində verilən dəyərin yerini (indeksini) ┃
+# ┃  tapmaq üçün istifadə olunur.                              ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyveler = ("alma", "banan", "portağal", "kivi", "nar")
+
+index_kivi = meyveler.index("kivi")
+
+print(index_kivi)
+
+# Output:
+# 3
---
+++ 044 index()/2.py
@@ -0,0 +1,16 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  eyni deyer bir neçe defe tekrar edilirse,                 ┃
+# ┃  ilk tapılanın indeksini verir                             ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+tekrar = ("a", "b", "c", "a", "d")
+
+index_a = tekrar.index("a")
+
+print(index_a)
+
+# Output:
+# 0
