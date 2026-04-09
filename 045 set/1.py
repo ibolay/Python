24038b8eab03f8b44724d839ed12008cada26724
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-09 22:32:50 +0400

    045 set - 1

---
+++ 045 set/1.py
@@ -0,0 +1,17 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  set - yaratmaq üçün {} bezekli möterizeden istifadə       ┃
+# ┃  etmək lazımdır.                                           ┃
+# ┃                                                            ┃
+# ┃  setdə nəticəni ekrana yazdırmaq istedikde her defe        ┃
+# ┃  deyerler öz yerini deyişesidi.                            ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyveler = {"alma", "banan", "portağal"}
+
+print(meyveler)
+
+# Output:
+# deyerlerin düzülüşü deyişir.
