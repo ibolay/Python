c969ebd1fbdd192a932e0128be4bedeecd2746a4
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-13 17:57:00 +0400

    057 values()

---
+++ 057 values()/1.py
@@ -0,0 +1,15 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  values() - dictionary-də olan bütün DƏYƏRLƏRİ qaytarır.   ┃
+# ┃                                                            ┃
+# ┃  Yəni yalnız sağ tərəfi göstərir.                          ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+telebe = {"ad": "Ali", "yas": 20}
+
+print(telebe.values())
+
+# Output:
+# dict_values(['Ali', 20])
