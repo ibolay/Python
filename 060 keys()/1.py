7bd0fc03d196e7e05e1427a2f63927e17ee83cd0
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-13 18:02:05 +0400

    060 keys()

---
+++ 060 keys()/1.py
@@ -0,0 +1,15 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  keys() - dictionary-də olan bütün AÇARLARI (key) verir.   ┃
+# ┃                                                            ┃
+# ┃  Yəni yalnız sol tərəfi qaytarır.                          ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+telebe = {"ad": "Ali", "yas": 20}
+
+print(telebe.keys())
+
+# Output:
+# dict_keys(['ad', 'yas'])
