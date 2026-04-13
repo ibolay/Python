d236727ad1704543aea79d055531be94ec9fba6b
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-13 14:14:44 +0400

    050 del

---
+++ 050 del/1.py
@@ -0,0 +1,17 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  del - seti və ya setin elementini silmək üçün istifadə    ┃
+# ┃  olunur.                                                   ┃
+# ┃                                                            ┃
+# ┃  Müəyyən elementi silmək üçün istifadə oluna bilər.        ┃
+# ┃                                                            ┃
+# ┃  Bütün seti silsən, artıq o dəyişən mövcud olmayacaq.      ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyveler = {"alma", "banan", "portağal"}
+
+del meyveler  # bütün set silindi
+
+# print(meyveler)  # ❌ error verər (çünki artıq yoxdur)
