ed13e5e363dcd1cb3ad8cf9729d473c9c90dbcb8
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-13 17:58:35 +0400

    058 items()

---
+++ 058 items()/1.py
@@ -0,0 +1,15 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  items() - dictionary-də olan HƏM key, HƏM value verir.    ┃
+# ┃                                                            ┃
+# ┃  Yəni hər ikisini cüt-cüt göstərir.                        ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+telebe = {"ad": "Ali", "yas": 20}
+
+print(telebe.items())
+
+# Output:
+# dict_items([('ad', 'Ali'), ('yas', 20)])
