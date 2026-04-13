8ecf05650fe788ee2f95511123b51e54ac9c1540
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-13 17:50:10 +0400

    055 dictionary - 5

---
+++ 055 dictionary/11.py
@@ -0,0 +1,15 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  keys() - metodu ile sadece açar sözleri elde edirik.      ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+telebe = {
+    "ad" : "Mehman",    "yas" : 20,    "seher" : "Gəncə"
+}
+
+print( telebe.keys() )
+
+# Output:
+# dict_keys(['ad', 'yas', 'seher'])
