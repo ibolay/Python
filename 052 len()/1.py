6d511e44bd2aea0bc24d5d892076ade19d2eab38
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-13 14:18:38 +0400

    052 len()

---
+++ 052 len()/1.py
@@ -0,0 +1,18 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  len() - setin içində neçə element olduğunu göstərir.      ┃
+# ┃                                                            ┃
+# ┃  Yəni setin uzunluğunu (element sayını) qaytarır.          ┃
+# ┃                                                            ┃
+# ┃  Təkrar dəyərlər sayılmır (setdə onsuz da olmur).          ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+meyveler = {"alma", "banan", "portağal", "alma"}
+
+print(len(meyveler))
+
+# Output:
+# 3
+# 'alma' təkrar olduğu üçün 1 dəfə sayıldı
