f3dae0d3f897b2d486c2df0f9d25f901bcaa52b2
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-14 20:00:00 +0400

    065 isdigit()

---
+++ 065 isdigit()/1.py
@@ -0,0 +1,14 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  isdigit() - string yalnız rəqəmlərdən ibarətdirsə True    ┃
+# ┃  qaytarır, yoxsa False.                                    ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+metn = "12345"
+
+print(metn.isdigit())
+
+# Output:
+# True
