325aec2800c4f41c66794547649d1c3cf9b60a02
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-14 20:00:00 +0400

    067 islower()

---
+++ 067 islower()/1.py
@@ -0,0 +1,14 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  islower() - string-də bütün hərflər kiçikdirsə True       ┃
+# ┃  qaytarır, əks halda False.                                ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+metn = "salam"
+
+print(metn.islower())
+
+# Output:
+# True
