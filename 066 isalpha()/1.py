91e8f40ae590ec8ffa094726b9449fdece9fbece
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-14 20:00:00 +0400

    066 isalpha()

---
+++ 066 isalpha()/1.py
@@ -0,0 +1,14 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  isalpha() - string yalnız hərflərdən ibarətdirsə True     ┃
+# ┃  qaytarır, yoxsa False.                                    ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+metn = "salam"
+
+print(metn.isalpha())
+
+# Output:
+# True
