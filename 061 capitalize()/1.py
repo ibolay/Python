f8e1a9881129d79533833f49975354be21ad0c8a
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-14 20:00:00 +0400

    061 capitalize()

---
+++ 061 capitalize()/1.py
@@ -0,0 +1,16 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  capitalize() - string-in (mətin) ilk hərfini böyük,       ┃
+# ┃  qalan bütün hərfləri isə kiçik edir.                      ┃
+# ┃                                                            ┃
+# ┃  Yalnız birinci hərf dəyişir.                              ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+ad = "aLi ibrahim"
+
+print(ad.capitalize())
+
+# Output:
+# Ali ibrahim
