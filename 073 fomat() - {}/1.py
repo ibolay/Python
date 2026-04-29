07a593c5de1dbc52238058f685844b81ed333a90
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-29 16:58:29 +0400

    073 format() - {} - 1

---
+++ 073 fomat() - {}/1.py
@@ -0,0 +1,16 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  format() ilə dəyişənləri string içində istifadə edirik.   ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+ad = "Ibrahim"
+yas = 18
+
+metn = "Menim adim {} ve yasim {} dir"
+
+print( metn.format(ad, yas) )
+
+# Output:
+# Menim adim Ibrahim ve yasim 18 dir
