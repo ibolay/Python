1e07318244f3492ed467a00f378a392e2c17cc70
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-29 17:50:23 +0400

    075 f

---
+++ 075 f/1.py
@@ -0,0 +1,22 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  """ """ (triple quotes) – çoxsətirli string yazmaq üçündür┃
+# ┃                                                            ┃
+# ┃  Alt-alta yazılan mətni xətasız saxlamağa imkan verir      ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+metn = """
+Bu bir string-dir
+və alt-alta yazila bilər
+heç bir xeta vermir
+       """
+
+print(metn)
+
+
+# Output:
+# Bu bir string-dir
+# və alt-alta yazıla bilər
+# heç bir xeta vermir
