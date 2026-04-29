c47c727c848eba2d01502e132565bbae5fb9799e
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-29 16:21:50 +0400

    070 round()

---
+++ 070 round()/1.py
@@ -0,0 +1,17 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  Ededleri yuvarlaqlaştırmaq üçün istifadə edilir.          ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+eded = input("Bir Eded Daxil Edin: ")
+eded = float(eded)
+eded = abs(eded)
+eded = round(eded)
+
+print( eded )
+
+# Output:
+# Bir Eded Daxil Edin: -20.6     <-- Misal
+# 21
