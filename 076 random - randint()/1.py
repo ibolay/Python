0d5cfc5e3aa56145059d43832f41e447efd05f5c
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-30 08:21:21 +0400

    076 random - randint()

---
+++ 076 random - randint()/1.py
@@ -0,0 +1,16 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  random modulunu çağırırıq, ve bu modula aid               ┃
+# ┃  metodları istifadə edirik.                                ┃
+# ┃                                                            ┃
+# ┃  randint() - teyin etdiyimiz ededlerin arasından           ┃
+# ┃  random bir ededi elde edirik.                             ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+import random
+
+Eded = random.randint( 1 , 36 )
+
+print( Eded )
