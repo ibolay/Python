bbdf8ba8265bfb8795b02eaf611d17e0acde002e
ibolicyber <ibrahimcyber4@gmail.com>
2026-04-30 08:28:18 +0400

    078 random - choice()

---
+++ 078 random - choice()/1.py
@@ -0,0 +1,14 @@
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                            ┃
+# ┃  choice - listin içinden random deyer verir.               ┃
+# ┃                                                            ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+import random
+
+meyve = [ "alma" , "armud" , "nar" , "heyva" ]
+
+RandomDeyer = random.choice(meyve)
+
+print( RandomDeyer )
